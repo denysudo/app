f8416f74f466f4a34df3305bdf4bcb7265bc9200
Denys <udovikdenys@gmail.com>
2024-09-17 17:11:47 +0200

    basic things

---
+++ src/Components/Header.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Header = () => {
+    return <header className='header'>
+        <img src= 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQvU_9D0nOTz2XfZzIcyOb1572yMWvNKP3EVg&usqp=CAU' />
+      </header>
+}
+
+export default Header;
---
+++ src/Components/Profile.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Profile = () => {
+    return <div className='content'>
+    <div>
+    <img src= 'https://www.ionos.at/digitalguide/fileadmin/DigitalGuide/Screenshots_2023/logo-best-practice.png'/>
+    </div>
+    <div>avatar + description</div>
+    <div>My posts
+      <div>New post</div>
+      <div>
+        <div>post1</div>
+        <div>post2</div>
+      </div>
+    </div>
+  </div>
+}
+
+export default Profile;
